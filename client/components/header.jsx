4c4c36cc670a708de6aa7ba9b330c9a0591337b5
kamrynmin <kamrynmin@gmail.com>
2020-02-06 11:25:54 +0000

    Updating viewing cart

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,7 +4,12 @@ function HeaderTitle(props) {
   return (
     <nav className="navbar fixed-top navbar-dark bg-dark">
       <a className="navbar-brand fas fa-dollar-sign col-5" style={{ color: 'white' }}>{props.text}</a>
-      <a className="col-1 fas fa-shopping-cart" style={{ color: 'white', width: '2rem' }}>  {props.cartCount} Items</a>
+      <div
+        onClick={props.viewCart}
+        className="text-right"
+        style={{ cursor: 'pointer' }}>
+        <a className="col-1 fas fa-shopping-cart" style={{ color: 'white', width: '2rem' }}>  {props.cartCount} Items</a>
+      </div>
     </nav>);
 
 }
